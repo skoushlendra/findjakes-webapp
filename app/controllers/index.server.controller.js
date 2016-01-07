cf991db6b579be97ef4dee9d339c8eabb5e278ac
Koushlendra Singh Parihar <skoushlendra@gmail.com>
2016-01-08 01:09:32 +0530

    basic implementation of findjakes index and our-story page

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,5 +1,11 @@
-exports.render = function(req, res) {
+exports.renderIndex = function(req, res) {
     res.render('index', {
+    	title: 'MEAN MVC',
+    	user: req.user ? req.user.username : ''
+    });
+};
+exports.renderOurStory = function(req, res) {
+    res.render('our-story', {
     	title: 'MEAN MVC',
     	user: req.user ? req.user.username : ''
     });
