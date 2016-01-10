c32c52ef317383190457d3ccf157b532f982e9b9
Koushlendra Singh Parihar <skoushlendra@gmail.com>
2016-01-11 00:41:49 +0530

    print logs for access controls

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -3,6 +3,7 @@ module.exports = function(app) {
     app.all('/', function(req, res, next) {
     	res.header("Access-Control-Allow-Origin", "*");
     	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    	console.log("Access controls added ");
     	next();
     });
     app.get('/', index.renderIndex);
